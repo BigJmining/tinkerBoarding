52b7e840c3a3e567ceb22d3513b296a9e7a57e8c
Jason Lohrey <40216394+BigJmining@users.noreply.github.com>
2019-02-27 22:02:28 -0800

    Update LS_detector.py
    Cleaned up some loose ends, added comments

--- LS_detector.py
+++ LS_detector.py
@@ -6,40 +6,35 @@ from time import sleep
 gpio.setwarnings(False)
 gpio.setmode(gpio.BCM)
 
+## photo sensor
 gpio.setup(26, gpio.IN)
 
+## switch and laser
 gpio.setup([16,21], gpio.OUT)
 
+## set LED pins to pwm
 gpio.PWM(23, 50)
 gpio.PWM(24, 50)
 gpio.PWM(25, 50)
 
-
-# gpio.setup(8,  gpio.OUT)
-# gpio.setup(12, gpio.OUT)
-# gpio.setup(16, gpio.OUT)
-
+## optional items added to GPOIO pins
 laser = 21
 switch = 16
 
+## LED lights
 light_g = 24
 light_b = 23
 light_r = 25
 
+## photo sensor
 sensor = 26
 
 statusBank = []
 
 all_lights = [23,24,25]
 
-#pwm = gpio.PWM(lite_pin, 50)
-#pwm.start(0)
-
-
 try:
     while 1:
-        # for i in range(10):
-        # gpio.output(all_lights, 0)
         pwm.start(0)
         scan = gpio.input(sensor)
         print(scan)
@@ -57,15 +52,11 @@ try:
             for x in range(100,0,-.25):
                 pwm.ChangeDutyCycle(x)
                 sleep(.2)
-
-            #gpio.output(light_g,1)
-            #gpio.output(laser, 0)
+                
             statusBank.append('on\n')
 
         if scan == 1:
             print('The light is off')
-
-            # gpio.output(all_lights,0)
 
             pwm = gpio.PWM(light_r ,50)
             pwm.start(0)
@@ -78,8 +69,6 @@ try:
                 pwm.ChangeDutyCycle(x)
                 sleep(.2)
 
-            #gpio.output(light_r,1)
-            #gpio.output(laser, 1)
             statusBank.append('off\n')
 
         sleep(.2)
@@ -88,7 +77,7 @@ except KeyboardInterrupt:
     gpio.output(all_lights,0)
     gpio.output(laser,0)
     pwm.stop()
-
+    ## printout a history of light sensor status
     print(statusBank)
     
 gpio.cleanup()
